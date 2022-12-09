d9358f06014239345aeeff8f530305ba4c06a734
Maximillian Lovett <s214274@students.aie.edu.au>
2022-12-09 16:31:08 +1100

    finalised game

---
+++ Assets/ThirdPersonShooter/Scripts/Common/AI/SpawnerManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ThirdPersonShooter.Scripts;
+using ThirdPersonShooter.Scripts.Entities.Player;
+
+using UnityEngine;
+
+namespace ThirdPersonShooter.AI
+{
+	public class SpawnerManager : MonoBehaviour
+	{
+		[Serializable]
+		public struct SpawnerGoals
+		{
+			public Spawner spawner;
+			public int pointThreshold;
+			public int pointEndThresholds;
+			public bool isDefault;
+
+			public void SetSpawnerState(bool _active) => spawner.gameObject.SetActive(_active);
+		}
+		
+		[SerializeField] private List<SpawnerGoals> spawners;
+
+		private PlayerEntity player;
+		
+		private void Awake()
+		{
+			player = GameManager.IsValid() ? GameManager.Instance.Player : FindObjectOfType<PlayerEntity>();
+			
+			foreach(SpawnerGoals spawner in spawners)
+				spawner.SetSpawnerState(spawner.isDefault);
+
+			player.onScoreUpdated += OnScoreUpdated;
+		}
+
+		private void OnDestroy()
+		{
+			player.onScoreUpdated -= OnScoreUpdated;
+		}
+
+		private void OnScoreUpdated(int _score)
+		{
+			foreach(SpawnerGoals spawner in spawners)
+				spawner.SetSpawnerState(_score >= spawner.pointThreshold);
+			
+			//foreach(SpawnerGoals spawner in spawners)
+			//	spawner.SetSpawnerState(_score >= spawner.pointEndThresholds);
+		}
+	}
+}
